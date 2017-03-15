1e919ef1e1cecd0effa50be9301c787f1363b998
陳泰儒 <hello19881990@gmail.com>
2017-03-15 16:20:39 +0800

    圖 終於對了 字終於放大了

---
+++ Assets/script/innputText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class innputText : MonoBehaviour {
+	[SerializeField]
+	private Text inputTxt;
+	// Use this for initialization
+	void Awake () {
+		// inputTxt.fontSize = 666;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
