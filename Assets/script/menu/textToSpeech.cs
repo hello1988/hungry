e596692bd283551777a3ad903c1b48a20a6654de
BlissChen <hello19881990@gmail.com>
2017-03-19 20:25:35 +0800

    文字轉聲音測試

---
+++ Assets/script/menu/textToSpeech.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class textToSpeech : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (translate ("我是泰儒"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator translate(string text)
+	{
+		// Remove the "spaces" in excess
+		Regex rgx = new Regex ("\\s+");
+		// Replace the "spaces" with "% 20" for the link Can be interpreted
+		string result = rgx.Replace (text, "%20");
+		string url = "http://translate.google.com/translate_tts?tl=en&q=" + result;
+		WWW www = new WWW (url);
+		yield return www;
+		AudioSource audio = this.GetComponent<AudioSource> ();
+		audio.clip = www.GetAudioClip (false, false, AudioType.MPEG);
+		audio.Play ();
+	}
+}
