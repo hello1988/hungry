5f22addd6faabe2b2d70a2232974250761464778
BlissChen <hello19881990@gmail.com>
2017-03-19 18:46:52 +0800

    補

--- Assets/script/pageMgr.cs
+++ Assets/script/pageMgr.cs
@@ -38,7 +38,7 @@ public class pageMgr : MonoBehaviour
 			page.SetActive (false);
 		}
 
-		int startPage = 5;
+		int startPage = 0;
 		pageList [startPage].SetActive (true);
 		pageBase showPage = pageList [startPage].GetComponent<pageBase> ();
 		showPage.onPageEnable ();
