7fdb4ead678b38e57f847ac086049ccb883d35ac
陳泰儒 <hello19881990@gmail.com>
2017-03-24 16:34:23 +0800

    新增Ｇ-1-4提示文字

---
+++ Assets/script/menu/assist3Ctrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class assist3Ctrl : MonoBehaviour 
+{
+	[SerializeField]
+	private Sprite[] spriteList; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void assistInit()
+	{
+		
+	}
+
+	public void setSpriteList( Sprite[] tipSpriteList )
+	{
+		spriteList = tipSpriteList;
+	}
+
+	public Sprite getTipSprite( int index )
+	{
+		if ((index < 0) || (index >= spriteList.Length)) {return null;}
+
+		return spriteList [index];
+	}
+
+}
---
+++ Assets/script/menu/showTip.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class showTip : MonoBehaviour, IPointerClickHandler 
+{
+	[SerializeField]
+	private assist3Ctrl assCtrl;
+	[SerializeField]
+	private int tipIndex;
+
+	private GameObject tipImage = null;
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnEnable()
+	{
+		StopAllCoroutines ();
+
+		if (tipImage != null) 
+		{
+			tipImage.SetActive (false);
+		}
+	}
+
+	public void OnPointerClick (PointerEventData eventData)
+	{
+		if (tipImage == null) 
+		{
+			tipImage = new GameObject ();
+			tipImage.name = string.Format ("tipImage{0}",(tipIndex+1));
+			tipImage.AddComponent<Image> ();
+			tipImage.transform.SetParent (transform);
+			tipImage.transform.localPosition = Vector3.zero;
+			tipImage.transform.localScale = Vector3.one;
+
+			RectTransform rect = tipImage.GetComponent<RectTransform> ();
+			RectTransform parentRect = GetComponent<RectTransform> ();
+			rect.sizeDelta = parentRect.sizeDelta;
+
+			Image img = tipImage.GetComponent<Image> ();
+			img.sprite = assCtrl.getTipSprite (tipIndex);
+		}
+
+		tipImage.SetActive (true);
+		StopAllCoroutines ();
+		StartCoroutine (resume());
+	}
+
+	public IEnumerator resume()
+	{
+		yield return new WaitForSeconds (10);
+
+		tipImage.SetActive (false);
+	}
+}
