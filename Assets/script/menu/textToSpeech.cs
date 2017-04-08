ebf841d82e0e4b9c71867525c84ad3f859d8adbb
BlissChen <hello19881990@gmail.com>
2017-04-08 22:45:30 +0800

    文字轉聲音

--- Assets/script/menu/textToSpeech.cs
+++ Assets/script/menu/textToSpeech.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text.RegularExpressions;
-using HoloToolkit.Unity;
 
 public class textToSpeech : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		// StartCoroutine (translate ("我是泰儒"));
-		ttsTest();
+		// StartCoroutine (translate (@"Hi I'm Bliss Chen.", @"en-gb"));
+		StartCoroutine (translate (@"我是又寧 哈哈哈", @"zh-tw"));
 	}
 	
 	// Update is called once per frame
@@ -17,23 +16,28 @@ public class textToSpeech : MonoBehaviour {
 		
 	}
 
-	IEnumerator translate(string text)
+	// google 檔案類型mpeg
+	// RRS  檔案類型ogg
+	IEnumerator translate(string text, string language)
 	{
+		string RRS_APIKey = "d0dd2c9bf5d94993bb96111e01e649ee";
+		string RRS_URL = "http://api.voicerss.org/?key={0}&hl={1}&src={2}&r=-4&c=OGG";
+
+		string GOOGLE_URL = "http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q={0}&tl={1}";
 		// Remove the "spaces" in excess
 		Regex rgx = new Regex ("\\s+");
-		// Replace the "spaces" with "% 20" for the link Can be interpreted
+
 		string result = rgx.Replace (text, "%20");
-		string url = "http://translate.google.com/translate_tts?tl=en&q=" + result;
+
+		// string url = string.Format (GOOGLE_URL, result, language);
+		string url = string.Format (RRS_URL, RRS_APIKey, language, result);
+
 		WWW www = new WWW (url);
 		yield return www;
 		AudioSource audio = this.GetComponent<AudioSource> ();
-		audio.clip = www.GetAudioClip (false, false, AudioType.MPEG);
+		// https://docs.unity3d.com/355/Documentation/ScriptReference/WWW.GetAudioClip.html
+		audio.clip = www.GetAudioClip (false, false, AudioType.OGGVORBIS);
 		audio.Play ();
 	}
 
-	private void ttsTest()
-	{
-		TextToSpeechManager tts = new TextToSpeechManager ();
-		tts.SpeakText("我是泰儒");
-	}
 }
